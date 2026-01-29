a173bc5213b659ea936b649445ef5e3ebdcec4c8
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-01-30 00:57:15 +0300

    GAME MANAGER
    Added Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Door levelDoor;
+    public Text funFactText;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void CollectSafe(string fact)
+    {
+        funFactText.text = fact;
+        funFactText.gameObject.SetActive(true);
+        levelDoor.ActivateDoor();
+    }
+}
