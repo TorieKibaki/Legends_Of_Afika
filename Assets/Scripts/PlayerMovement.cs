b8c1269a510305f4068acc7db7971bef126d80ea
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-02-02 04:31:47 +0300

    Updated Player Movement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,47 @@
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 10f;
-
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
     private Rigidbody2D rb;
     private bool isGrounded;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             isGrounded = true;
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+
+    void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             isGrounded = false;
+        }
     }
 }
--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -9,18 +9,8 @@ public class PlayerRespawn : MonoBehaviour
         startPosition = transform.position;
     }
 
-    [System.Obsolete]
     public void Respawn()
     {
-        // 1. Move the player to your new starting position
-        transform.position = new Vector3(-10f, -3f, 0f);
-
-        // 2. Find and reset tiles (Updated for Unity 6)
-        DisappearingTile[] allTiles = GameObject.FindObjectsByType<DisappearingTile>(FindObjectsSortMode.None);
-
-        foreach (DisappearingTile tile in allTiles)
-        {
-            tile.ResetTile();
-        }
+        transform.position = startPosition;
     }
 }
