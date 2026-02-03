9dffb23c237c9d924c500bc9ecc8215ee1ee7216
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-02-03 10:54:31 +0300

    Updated Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public bool hasSafeCollectible = false;
-
+    public bool hasSafeCollectible;
 
     void Awake()
     {
         if (instance == null)
+        {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
         else
+        {
             Destroy(gameObject);
+        }
     }
 
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        hasSafeCollectible = false;
+    }
 
     public void CollectSafe()
     {
         hasSafeCollectible = true;
-    }
-
-
-    public void ResetLevel()
-    {
-        hasSafeCollectible = false;
     }
 }
