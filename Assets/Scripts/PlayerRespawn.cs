b1926888151b644afc6d8e4143d9172921fe4a6a
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-02-03 10:56:11 +0300

    Updated the PLayer Respawn

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class PlayerRespawn : MonoBehaviour
 {
     private Vector3 startPosition;
+    private Rigidbody2D rb;
 
     void Start()
     {
         startPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Respawn()
     {
         transform.position = startPosition;
+
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
     }
 }
