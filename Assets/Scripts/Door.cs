167817b30e42ffc46a4713008b9390c2c60bf1a5
TOKIBA7 <107519070+TorieKibaki@users.noreply.github.com>
2026-02-03 11:14:06 +0300

    Update Door.cs

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class Door : MonoBehaviour
 {
+    [Header("Next Level")]
     public string nextSceneName;
 
+    [Header("Effects (Unlocked Only)")]
+    public GameObject doorParticles;
+    public AudioClip doorSound;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-
         if (GameManager.instance.hasSafeCollectible)
         {
+            if (doorSound != null)
+                AudioSource.PlayClipAtPoint(doorSound, transform.position);
+
+            if (doorParticles != null)
+                Instantiate(doorParticles, transform.position, Quaternion.identity);
+
             SceneManager.LoadScene(nextSceneName);
         }
         else
